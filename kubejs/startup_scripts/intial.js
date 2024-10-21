b57e6c7184d7f22ee04623fa52b4d582feda66b2
sayeeed <chipsoud@hotmail.com>
2024-10-20 21:13:50 -0500

    Changed menu title and in-game pack added item names

--- kubejs/startup_scripts/intial.js
+++ kubejs/startup_scripts/intial.js
@@ -2,4 +2,4 @@ StartupEvents.registry("item", (event) => {
   event.create("greg_icon");
 });
 
-Platform.mods.kubejs.name = "GregTech: Revolutions - v0.1.0-alpha";
+Platform.mods.kubejs.name = "GregTech: Revolutions";
